2fb9beb1cfbc61f99801e72e786d1f824b6e267e
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-07-31 18:30:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,6 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
 df = pandas.DataFrame(my_catalog)
+
+# temp write the dataframe to the page so I Can see what I am working with
+streamlit.write(df)
