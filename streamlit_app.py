77e358a6b6d582c4a8eb7a7b1cc2f068e144f1e1
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-07-31 18:33:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,5 @@ streamlit.write(df)
 # put the first column into a list
 color_list = df[0].values.tolist() 
 print(color_list)
+
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
